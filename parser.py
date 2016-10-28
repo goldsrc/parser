4fafd60e934058cb475738acc32255e5bb4afc7c
mbeheshtis <mohammad@payaneh.com>
2016-10-28 19:16:13 +0330

    minor bug fix

--- parser.py
+++ parser.py
@@ -1,20 +1,23 @@
+#importing required libraries
 from flask import request,Flask,Response
 from nltk.tokenize import word_tokenize
 import nltk,json,argparse
 from itertools import izip
 
+#initialize flask app
 app = Flask(__name__)
 @app.route('/parse', methods=['GET', 'POST'])
 
-def test():
-    dt=''
+#flask main method
+def tagger():
+    tags=[{}]
     if request.method == 'POST':
         sentence = request.form['sent']
         tokens = word_tokenize(sentence)
         tags = nltk.pos_tag(tokens)
-        dt=json.dumps(fixer(tags),indent=4, sort_keys=True)
-    return Response(dt, mimetype='text/json')
+    return Response(json.dumps(fixer(tags),indent=4, sort_keys=True), mimetype='text/json')
 
+#convert list to dictionary
 def fixer(lst):
     retval=[]
     for item in lst:
@@ -24,11 +27,10 @@ def fixer(lst):
     return retval
 
 if __name__ == '__main__':
-    #----------- Parsing Arguments ---------------
+    #Parsing Arguments
     p = argparse.ArgumentParser()
     p.add_argument("--port", help="Port (default: 5000)")
     args = p.parse_args()
+	#setting port for the service
     port = int(args.port) if args.port else 5000
-    if not args.model:
-        print "Usage: parser.py [--port 1234]"
     app.run(port=port)
